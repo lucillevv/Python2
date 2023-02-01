0f7e4d5baebf10a26d13e11877a47813342017b9
Lucille Van Vooren <lucille.van.vooren@gmail.com>
2023-02-01 20:15:54 +0100

    copy folders

---
+++ modules/pcinput.py
@@ -0,0 +1,37 @@
+def getFloat(prompt):
+    while True:
+        try:
+            num = float(input(prompt))
+        except ValueError:
+            print("Geen getal -- probeer het opnieuw")
+            continue
+        return num
+
+
+def getInteger(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+        except ValueError:
+            print("Geen geheel getal -- probeer het opnieuw")
+            continue
+        return num
+
+
+def getString(prompt):
+    line = input(prompt)
+    return line.strip()
+
+
+def getLetter(prompt):
+    while True:
+        line = input(prompt)
+        line = line.strip()
+        line = line.upper()
+        if len(line) != 1:
+            print("Geef precies een letter in")
+            continue
+        if line < "A" or line > "Z":
+            print("Geef een letter van het alfabet in")
+            continue
+        return line
